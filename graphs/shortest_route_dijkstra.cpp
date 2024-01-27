aca5b991ac96f599b27be41f132e655c921e5e88
Ram Kishan <ram.kishan.v.vooka@oracle.com>
2024-01-27 17:12:50 +0530

    x

--- graphs/shortest_route_dijkstra.cpp
+++ graphs/shortest_route_dijkstra.cpp
@@ -3,16 +3,16 @@ using namespace std;
 #define ll long long
 #define INF 1000000000000000
 
-void dijkstra(vector<vector<pair<ll,ll>>> &adj, vector<ll> &dist){
+void dijkstra(vector<vector<pair<ll,ll>>> &adj, vector<ll> &dist, vector<bool> &vis){
 	priority_queue<pair<ll,ll>, vector<pair<ll,ll>>, greater<pair<ll,ll>>> pq;
 	dist[1]=0;
 	pq.push({0,1});
-	ll v,u,w,d;
+	ll v,u,w;
 	while(!pq.empty()){
 		u = pq.top().second;
-		d = pq.top().first;
 		pq.pop();
-		if(d>dist[u]) continue;
+		if(!vis[u]){
+			vis[u]=true;
 			for(int j=0;j<adj[u].size();j++){
 				v = adj[u][j].first;
 				w = adj[u][j].second;
@@ -21,6 +21,8 @@ void dijkstra(vector<vector<pair<ll,ll>>> &adj, vector<ll> &dist){
 					pq.push({dist[v],v});
 				}
 			}
+		}
+			
 	}
 	return;
 }
@@ -35,6 +37,7 @@ int main(){
 		adj[a].push_back({b,c});
 	}
 	vector<ll> dist(n+1,INF);
-	dijkstra(adj,dist);
+	vector<bool> vis(n+1,false);
+	dijkstra(adj,dist,vis);
 	for(int i=1;i<=n;i++) cout<<dist[i]<<" ";
 }
